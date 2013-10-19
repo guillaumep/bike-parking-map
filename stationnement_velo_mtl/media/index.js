36888ffc8866e0fdfbc650c132f59e9a32651cdd
Guillaume Pratte <gpratte@outboxtechnology.com>
2013-10-19 16:29:09 -0400

    redraw on adding point

--- stationnement_velo_mtl/media/index.js
+++ stationnement_velo_mtl/media/index.js
@@ -56,11 +56,13 @@ var heatmapLayer = L.TileLayer.heatMap({
     }
 });
 
+heatmapLayer.on('loading', function(){
+    $.getJSON("/requests", function(data) {
+       heatmapLayer.setData(data); 
+    })
+})
 //var data = [{lat: 45.764, lon:-73.8974, value: 1},{lat: 45.867, lon:-73.8576, value: 1}, {lat: 45.8437, lon:-73.976, value: 1}];
 
-$.getJSON("/requests", function(data) {
-   heatmapLayer.setData(data); 
-})
 
 
 
@@ -123,6 +125,11 @@ map.on("click", function(e) {
         data[2] = {name: "lat", value: lat.toString()};
         data[3] = {name: "lng", value: lng.toString()};
         submitValue(data, popup);
+        $.getJSON("/requests", function(data) {
+           heatmapLayer.setData(data); 
+           heatmapLayer.redraw();
+        })
+
         return false;
     });
 });
