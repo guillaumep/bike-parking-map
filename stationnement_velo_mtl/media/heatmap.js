fef78604fe41f71583cfb934f538a6f98337abcf
Françoise Provencher <francoiseprovencher@gmail.com>
2013-10-19 15:17:35 -0400

    added heatmap.js

--- stationnement_velo_mtl/media/heatmap.js
+++ stationnement_velo_mtl/media/heatmap.js
@@ -1,1 +1,7 @@
-Heatmap layer for Stationnement Velo 
+//Heatmap layer
+
+//Get data from Mongo DB
+
+//Parse data into the following format [{lat: 33.5363, lon:-117.044, value: 1},{lat: 33.5608, lon:-117.24, value: 1},..]
+
+// Use this data in a Leaflet Heatmap
