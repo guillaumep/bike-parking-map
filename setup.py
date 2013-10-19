710c8af573bc57945bd994de1a355d671c5d760e
Mathieu Leduc-Hamel <marrakis@gmail.com>
2013-10-19 15:30:17 -0400

    added mongodb backend

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setup(
     include_package_data=True,
     packages=find_packages(exclude=['ez_setup']),
     install_requires=[
-        "bottle"
+        "bottle",
+        "requests",
+        "pymongo",
     ])
--- stationnement_velo_mtl/__main__.py
+++ stationnement_velo_mtl/__main__.py
@@ -1,19 +1,24 @@
+import sys
 import os
+import pymongo
 
 from bottle import route, run, static_file, post, request
 
 dirname = os.path.dirname(__file__)
+db = None
 
 @route('/')
 def index():
     return static_file("index.html", os.path.join(dirname, "media"))
 
+
 @post('/submit')
 def submit(*args, **kwargs):
-    request.forms.comment
-    request.forms.purpose
-    return {}
-
+    db["requests"].insert(dict(
+        comment = request.forms.comment,
+        purpose = request.forms.purpose
+    ))
+    return "ok"
 
 @route('/media/<path:path>')
 def callback(path):
@@ -21,4 +26,9 @@ def callback(path):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        raise Exception("Missing password")
+    conn = "mongodb://velomtl:%s@ds049888.mongolab.com:49888/velomtl"
+    conn = conn % sys.argv[1]
+    db = pymongo.MongoClient(conn)["velomtl"]
     run(host='localhost', port=8080)
